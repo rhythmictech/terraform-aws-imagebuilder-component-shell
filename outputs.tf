8787bbdbc465eafa0bfd1de8f89ffaaab19028a2
Scott Miller <scott@millergeek.xyz>
2020-07-01 14:31:48 -0400

    output depends_on

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 output "component_arn" {
   description = "ARN of the EC2 Image Builder Component"
   value       = aws_cloudformation_stack.this.outputs["ComponentArn"]
+
+  depends_on = [
+    aws_cloudformation_stack.this
+  ]
 }
