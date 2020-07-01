bfdfc7b550019a80f9989182003b7719b541d6fd
Scott Miller <scott@millergeek.xyz>
2020-07-01 15:13:47 -0400

    compose arn

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+}
+
 output "component_arn" {
   description = "ARN of the EC2 Image Builder Component"
-  value       = aws_cloudformation_stack.this.outputs["ComponentArn"]
-
-  depends_on = [
-    aws_cloudformation_stack.this
-  ]
+  value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:component/${lower(var.name)}/${var.component_version}/1"
 }
